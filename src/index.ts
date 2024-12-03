9dc89d3f1cc640618604c578b471852d94d3edd6
pangz <pangz.lab@gmail.com>
2024-12-03 23:11:57 +0900

    updated export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type { EventData } from "verus-zmq-client";
-export { Request, Response, NextFunction } from "express";
+export type { Request, Response, NextFunction } from "express";
 export * from './lib/HttpServer';
 export * from './lib/WsServer';
 export * from './lib/RpcService';
